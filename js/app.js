63926d89e3b50fd832082c287ac18c1159817d69
alinaxi1 <55397901+alinaxi1@users.noreply.github.com>
2020-02-03 10:39:43 -0500

    almost there

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;
 let turn;
@@ -21,6 +30,7 @@ function init() {
     "", "", ""
   ];
   turn = "X";
+  win = null;
 
   render();
 }
@@ -40,6 +50,23 @@ function takeTurn(e) {
 
   board[index] = turn;
   turn = turn === "X" ? "O" : "X";
+  win = getWinner();
 
   render();
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner;
 }
